be690a6b02ea62d9fe9310d6fd05df863fd86514
strawhatcrue1 <84302030+strawhatcrue1@users.noreply.github.com>
2021-10-16 18:23:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/moviedb/helper/Const.java
@@ -0,0 +1,9 @@
+package com.example.moviedb.helper;
+
+public class Const {
+
+    public static final String API_KEY = "89bb487fcbd6e731f1964d4cafaf111d";
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String IMG_URL = "https://image.tmdb.org/t/p/w500/";
+
+}
---
+++ app/src/main/java/com/example/moviedb/viewmodel/MovieViewModel.java
@@ -0,0 +1,47 @@
+package com.example.moviedb.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.moviedb.model.Movies;
+import com.example.moviedb.model.NowPlaying;
+import com.example.moviedb.repositories.MovieRepository;
+
+public class MovieViewModel extends AndroidViewModel {
+
+    private MovieRepository repository;
+
+    public MovieViewModel(@NonNull Application application) {
+        super(application);
+        repository = MovieRepository.getInstance();
+    }
+
+    //==Begin of ViewModel getMoviebyID
+
+    private MutableLiveData<Movies> resultGetMovieById = new MutableLiveData<>();
+    public void getMovieById(String movieId){
+        resultGetMovieById = repository.getMovieData(movieId);
+    }
+    public LiveData<Movies> getResultGetMovieById(){
+        return resultGetMovieById;
+    }
+
+    //==End of ViewModel getMoviebyID
+
+    //==Begin of ViewModel getNowPlaying
+
+    private MutableLiveData<NowPlaying> resultGetNowPlaying = new MutableLiveData<>();
+    public void getNowPlaying(){
+        resultGetNowPlaying = repository.getNowPlayingData();
+    }
+    public LiveData<NowPlaying> getResultNowPlaying(){
+        return resultGetNowPlaying;
+    }
+
+    //==End of ViewModel getNowPlaying
+
+}
